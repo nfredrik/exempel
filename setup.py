5fc2c01d6c0738acf1117827c85d56c3d0781bb7
Fredrik Svärd <fredrik.svard@svenskaspel.se>
2021-02-14 15:35:05 +0100

    first draft

---
+++ setup.py
@@ -0,0 +1,21 @@
+# setup.py
+from setuptools import find_packages, setup
+
+
+setup(
+    name='exempel',
+    version='0.0.1',
+    python_requires='>=3.6,<3.7',
+    packages=find_packages(),
+    classifiers=[
+        'Development Status :: 1 - Planning',
+
+        'Intended Audience :: Developers',
+
+        'Programming Language :: Python :: 3.6',
+    ],
+    author='Freddan',
+    author_email='nfredrik@hotmail.com',
+    license='MIT',
+    url='https://github.com/nfredrik/exempel.git',
+)
